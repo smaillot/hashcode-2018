2125bed0468db5a0295a2e8bdefdb6af343934c2
Seboran <nirinarabeson@outlook.fr>
2018-03-01 19:03:37 +0100

    list of vehicles

--- qualification/loaded_input.py
+++ qualification/loaded_input.py
@@ -4,6 +4,7 @@ from solution import generate_solution
 class Loaded_input:
     def __init__(self, args):
         self.args = args
+        self.list_vehicles = self.F * [[0, 0]]
 
     def generate_solution(self, seed):
         
