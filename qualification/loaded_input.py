017213ee0fe203b6d7c0a626e3519f0e4c000ede
Stéphane Maillot <s.maillot2@gmail.com>
2018-03-01 19:05:51 +0100

    add IO

--- qualification/loaded_input.py
+++ qualification/loaded_input.py
@@ -2,8 +2,14 @@ from IO import *
 from solution import generate_solution
 
 class Loaded_input:
-    def __init__(self, args):
-        self.args = args
+    def __init__(self, params, array):
+        self.rows = params[0]
+        self.cols = params[1]
+        self.vehicules = params[2]
+        self.rides = params[3]
+        self.bonus = params[4]
+        self.steps = params[5]
+        self.rides = array
 
     def generate_solution(self, seed):
         
