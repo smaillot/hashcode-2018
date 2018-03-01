1b15846dc98b3930ebee3503ac557d41e3d59446
Stéphane Maillot <s.maillot2@gmail.com>
2018-03-01 20:01:05 +0100

    rides feasability

--- utils.py
+++ utils.py
@@ -9,3 +9,14 @@ import numpy as np
 def distance(depart, arrivee):
     difference = arrivee - depart
     return np.abs(difference[0]) + np.abs(difference[1])
+
+def filter_rides_start_in_time(start, end, rides_array):
+
+    rides = np.copy(rides_array)
+    return [r for r in rides if r[4]>=start and r[4]<=end]
+
+def list_possible_rides(x, y, t, max_steps, rides_array):
+
+    rides = filter_rides_start_in_time(t, t+max_steps, rides_array)
+    pose = np.array([x, y])
+    return [r for r in rides if distance(pose, np.array([r[0], r[1]])) < r[4] - start]
